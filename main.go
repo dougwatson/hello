4d2bfbc82f3a8f2a14ad9809e0e60703e00ed111
Doug Watson <dougwatson@gocoder.io>
2023-01-13 06:18:00 -0600

    added remote bar package

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"./morestrings"
+	"github.com/dougwatson/bar"
 )
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello, world."))
+	println("printint from remote imported bar package=",bar.Hello())
 }
