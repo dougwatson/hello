883ad470400b424bf038eb5f3f2150cecf11142e
Doug Watson <dougwatson@gocoder.io>
2023-01-18 18:46:31 -0600

    take args

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/dougwatson/hello/morestrings"
 	"github.com/dougwatson/bar"
 )
 func main() {
-	fmt.Println(morestrings.ReverseRunes("Hello, world."))
+	word:="Hello,World"
+	if len(os.Args)>1{
+		word=os.Args[1]
+	}
+	fmt.Println(morestrings.ReverseRunes(word))
 	println("import pkg:",bar.Hello())
 }
