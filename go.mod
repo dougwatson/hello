ad5a1c0a1e64466ba249fac5979a454c80a47b99
Doug Watson <dougwatson@gocoder.io>
2023-02-15 19:25:35 -0600

    add easyfs

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/dougwatson/xhello
 go 1.19
 
 require github.com/dougwatson/xbar v0.0.1
+require github.com/gocoderpro/easyfs v0.0.4
+
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"github.com/dougwatson/xhello/morestrings"
 	"github.com/dougwatson/xbar"
+	"github.com/gocoderpro/easyfs"
 )
 func main() {
 	word:="Hello,World"
@@ -13,4 +14,11 @@ func main() {
 	}
 	fmt.Println(morestrings.ReverseRunes(word))
 	println("import pkg:",xbar.Hello())
+	fs:=GetFS()
+	b,err:=fs.ReadFile("home/src/github.com/dougwatson/xhello")
+	if err!=nil {
+	   println("error: "+err)
+	}
+	println("b=",string(b))
+	
 }
